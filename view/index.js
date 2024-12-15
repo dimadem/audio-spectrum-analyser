a7645e1f8eae234cedfd1ec9da95b95077c739c0
dimadem <iamdimadem@gmail.com>
2024-12-15 09:27:10 +0000

    add: fixed window size, log visualisation, smoothnes, optimise rendering

--- view/index.js
+++ view/index.js
@@ -11,7 +11,7 @@ class spectrum_analyser_View extends HTMLElement {
         {
             const dataDisplay = this.querySelector('#visualization');
             if (!dataDisplay) return;
-            this.updateVisualization(value.peakMagnitudes);
+            this.updateVisualization(value.magnitudes);
         };
 
         // take a look at spectrum and realign the visualization
@@ -36,27 +36,59 @@ class spectrum_analyser_View extends HTMLElement {
 
         //! calculate the pixel size
         //1. magnitudes.length -> total number of frequencies
-        const totalFrequencies = magnitudes.length;
         //2. size of each pixel related to screen width
-        const pixelSize = Math.max(1, Math.floor(width / totalFrequencies)); 
-        
-        //! todo convert linear scale to logarithmic scale
+        const toLog = (value, min, max) => min * Math.pow(max/min, (value - min) / (max - min));
+        const smoothingFactor = 0.97;
 
-        // draw the line
-        magnitudes.forEach((magnitude, index) => {
-            const x = index * pixelSize;
-            const pixelHeight = Math.ceil(magnitude * height);
+        if (!this.previousMagnitudes) {
+            this.previousMagnitudes = new Float32Array(magnitudes.length).fill(0);
+        }
+
+        const totalFrequencies = magnitudes.length;
+        const pixelSize = Math.max(1, Math.floor(width / totalFrequencies));
+        const logMagnitudes = new Float32Array(totalFrequencies);
+
+        ctx.fillStyle = `rgba(0, 0, 0, 1.0)`;
+        for (let i = 1; i < totalFrequencies; i++) {
+            const logIndex = toLog(i, 1, totalFrequencies - 1);
+            const low = Math.floor(logIndex);
+            const high = Math.ceil(logIndex);
+            const weight = logIndex - low;
             
-            // draw the pixel
-            for (let y = height; y > height - pixelHeight; y -= pixelSize) {
-                ctx.fillStyle = `rgba(0, 0, 0, 1.0)`;
-                ctx.fillRect(x, y - pixelSize, pixelSize, pixelSize);
-            }
-        });
+            
+            const currentValue = magnitudes[low] + (magnitudes[high] - magnitudes[low]) * weight;
+            logMagnitudes[i] = currentValue * (1 - smoothingFactor) + this.previousMagnitudes[i] * smoothingFactor;
+            
+            const x = i * pixelSize;
+            const pixelHeight = Math.ceil(logMagnitudes[i] * height);
+            ctx.fillRect(x, height - pixelHeight, pixelSize, pixelHeight);
+        }
         ctx.stroke();
     }
 
-    
+    connectedCallback() {
+        this.paramListener = (event) => {
+            const horisontal_slider = this.querySelector('#horisontal_slider');
+            if (horisontal_slider)
+                horisontal_slider.value = event.value;
+
+            // const vertical_slider = this.querySelector('#vertical_slider');
+            // if (vertical_slider)
+            //     vertical_slider.value = event.value;
+        }
+
+        // Attach a parameter listener that will be triggered when any param is moved
+        this.patchConnection.addAllParameterListener (this.paramListener);
+
+        for (const param of this.querySelectorAll (".gui"))
+        {
+            // When one of our sliders is moved, this will send the new value to the patch.
+            param.oninput = () => this.patchConnection.sendEventOrValue (param.id, param.value);
+
+            // for each slider, request an initial update, to make sure it shows the right value
+            this.patchConnection.requestParameterValue (param.id);
+        }
+    }
 
 
     getHTML() {
